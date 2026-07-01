2ea2d176bcf32b6351e12de09de2735570125fd7
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-07-01 15:24:44 -0400

    added md -> html parsing functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,11 @@
+import re
 from textnode import TextNode, TextType
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
@@ -18,4 +25,86 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                     if parts_list[i] != "":
                         new_nodes.append(TextNode(parts_list[i], text_type))
 
+    return new_nodes
+
+# def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+#     new_nodes = []
+#     for node in old_nodes:
+#         if node.text_type != TextType.TEXT:
+#             new_nodes.append(node)
+#             continue
+#         parts = re.split(r"(!\[[^\[\]]*\]\([^\(\)]*\))", node.text)
+#         if len(parts) == 1:
+#             new_nodes.append(node)
+#             continue
+#         for i in range(len(parts)):
+#             if parts[i] == "":
+#                 continue
+#             if i % 2 == 0:
+#                 new_nodes.append(TextNode(parts[i], TextType.TEXT))
+#             else:
+#                 image_parts = extract_markdown_images(parts[i])
+#                 new_nodes.append(TextNode(image_parts[0][0], TextType.IMAGE, image_parts[0][1]))
+#     return new_nodes
+
+# attempting solution implementation
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(
+                TextNode(
+                    image[0],
+                    TextType.IMAGE,
+                    image[1],
+                )
+            )
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+        for link in links:
+            sections = original_text.split(f"[{link[0]}]({link[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("invalid markdown, link section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+    return new_nodes
+
+def text_to_textnodes(text):
+    new_nodes = split_nodes_delimiter([TextNode(text, TextType.TEXT)], "**", TextType.BOLD)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
     return new_nodes
