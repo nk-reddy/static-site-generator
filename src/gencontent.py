b6cce37d536189a70629ccd21e6d71362c4b9fbb
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-07-01 23:13:59 -0400

    deployed to github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
     raise Exception("no h1 header found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         user_markdown = f.read()
@@ -22,6 +22,8 @@ def generate_page(from_path, template_path, dest_path):
     user_title = extract_title(user_markdown)
     user_template = user_template.replace("{{ Title }}", user_title)
     user_template = user_template.replace("{{ Content }}", user_html)
+    user_template = user_template.replace('href="/', f'href="{basepath}')
+    user_template = user_template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "" and not os.path.exists(dest_dir_path):
@@ -31,16 +33,16 @@ def generate_page(from_path, template_path, dest_path):
         f.write(user_template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     inner_paths = os.listdir(dir_path_content)
     for path in inner_paths:
         combined_path = os.path.join(dir_path_content, path)
         if os.path.isfile(combined_path):
             if path.endswith(".md"):
                 destination_path = os.path.join(dest_dir_path, path.replace(".md", ".html"))
-                generate_page(combined_path, template_path, destination_path)
+                generate_page(combined_path, template_path, destination_path, basepath)
         else:
             destination_path = os.path.join(dest_dir_path, path)
-            generate_pages_recursive(combined_path, template_path, destination_path)
+            generate_pages_recursive(combined_path, template_path, destination_path, basepath)
 
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from copystatic import copy_fresh_contents
 from gencontent import generate_pages_recursive
+import sys
 
 def main():
-    copy_fresh_contents("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    copy_fresh_contents("static", "docs")
+
+    if len(sys.argv) < 2:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
+    
+    generate_pages_recursive("content", "template.html", "docs", basepath)
+
 
 main()
